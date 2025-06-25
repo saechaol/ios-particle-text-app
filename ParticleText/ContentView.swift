5992e0acde4d717264b396bc1c51f6792a13e33a
saechaol <33850575+saechaol@users.noreply.github.com>
2025-06-24 23:36:56 -0700

    Initial Commit

---
+++ ParticleText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParticleText
+//
+//  Created by 세차오 루카스 on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParticleText/ParticleTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleTextApp.swift
+//  ParticleText
+//
+//  Created by 세차오 루카스 on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ParticleTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
